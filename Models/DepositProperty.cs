2100771574be948962e94c029a6196d227635ba2
Hamid Fakih <neverwinter745@gmail.com>
2022-12-09 04:08:41 +0000

    Deposit models and class / Optimization of code/ login Hamid

---
+++ Models/DepositProperty.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace easytransfer.Models
+{
+    public class Deposit
+    {
+        public int Uid { get; set; }
+        public decimal Amount { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,12 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using easytransfer.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace easytransfer.Pages
 {
     public class LoginModel : PageModel
     {
+        DatabaseContext _Context;
+
+        public LoginModel(DatabaseContext databasecontext)
+        {
+            _Context = databasecontext;
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            public int Uid { get; set; }
+            public string Password { get; set; }
+            public bool RememberMe { get; set; }
+        }
+
         public void OnGet()
         {
+        }
+
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = _Context.easyuser.SingleOrDefault(
+                u => u.uid == Input.Uid &&
+                     u.password == Input.Password
+            );
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Invalid ID or password");
+                return Page();
+            }
+
+            return RedirectToPage("AllUsers");
         }
     }
 }
