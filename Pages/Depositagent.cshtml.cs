4e2de0e55927b0dc1be614992cc3e2979732fa04
Hamid Fakih <neverwinter745@gmail.com>
2022-12-11 08:23:13 +0000

    Hamid Deposit Agent / Transaction History for agent only and modifications to models and optimization

---
+++ Pages/Depositagent.cshtml.cs
@@ -0,0 +1,69 @@
+using easytransfer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace easytransfer.Pages
+{
+    public class DepositAgentModel : PageModel
+    {
+        [BindProperty]
+        public TransactionHistory TransactionHistory { get; set; }
+
+        DatabaseContext _Context;
+
+        public DepositAgentModel(DatabaseContext databasecontext)
+        {
+            _Context = databasecontext;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            // Check if user is of type 2
+            var sender = _Context.easyuser.SingleOrDefault(x => x.uid == TransactionHistory.sender);
+            if (sender.usertype != 2)
+            {
+                // Add error message
+                ModelState.AddModelError(string.Empty, "Only Agent can send to normal user");
+                return;
+            }
+
+            // Check if receiver is of type 1
+            var receiver = _Context.easyuser.SingleOrDefault(x => x.uid == TransactionHistory.receiver);
+            if (receiver.usertype != 1)
+            {
+                // Add error message
+                ModelState.AddModelError(string.Empty, "Only Agent can send to normal user");
+                return;
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
+
+            // Update sender's balance
+            sender.balance = sender.balance - TransactionHistory.amount;
+            _Context.Entry(sender).Property(x => x.balance).IsModified = true;
+
+            // Update receiver's balance
+            receiver.balance = receiver.balance + TransactionHistory.amount;
+            _Context.Entry(receiver).Property(x => x.balance).IsModified = true;
+
+            TransactionHistory.transactiondate = System.DateTime.Now;
+            TransactionHistory.transactiontype = 1;
+
+            var result = _Context.Add(TransactionHistory);
+            _Context.SaveChanges();
+
+            // Redirect to AllUsers page
+            Response.Redirect("TransactionHistoryAgent");
+        }
+
+    }
+}
---
+++ Pages/TransactionHistoryAgent.cshtml.cs
@@ -0,0 +1,62 @@
+using easytransfer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace easytransfer.Pages
+{
+    public class TransactionListModel : PageModel
+    {
+        private readonly DatabaseContext _context;
+
+        public TransactionListModel(DatabaseContext databaseContext)
+        {
+            _context = databaseContext;
+        }
+
+        public List<TransactionHistory> TransactionList { get; set; }
+        public List<EasyUser> EasyUserList { get; set; }
+
+        public void OnGet()
+        {
+            // Initialize the TransactionList and EasyUserList properties
+            TransactionList = new List<TransactionHistory>();
+            EasyUserList = new List<EasyUser>();
+
+            // Populate the TransactionList property with a list of transactions
+            TransactionList.AddRange(GetTransactionList());
+        }
+
+        private List<TransactionHistory> GetTransactionList()
+        {
+            // Get the transactions and users from the database
+            var transactions = _context.transactionhistory.ToList();
+            var users = _context.easyuser.ToList();
+
+            // Join the TransactionHistory and EasyUser models on the senderId and receiverId properties
+            var joined =
+            from th in transactions
+            join sender in users on th.sender equals sender.uid
+            join receiver in users on th.receiver equals receiver.uid
+
+            select new { th, sender, receiver };
+
+
+            // Filter the joined table by usertype
+            var filtered = joined.Where(x => x.sender.usertype == 2 && x.receiver.usertype == 1);
+
+            // Return the filtered transactions as a list
+            return filtered.Select(x => new TransactionHistory
+            {
+                trhisid = x.th.trhisid,
+                sender = x.sender.uid,
+                receiver = x.receiver.uid,
+                amount = x.th.amount,
+                transactiondate = x.th.transactiondate
+            }).ToList();
+        }
+    }
+}
