330408b3c9fe10e2f639ba5ee43985ae0ddd82c2
Hamid Fakih <neverwinter745@gmail.com>
2022-12-11 23:10:38 +0000

    Sending check balance and Usertype 1 to usertype 1 only BAH KOUASSI

--- Pages/Sending.cshtml.cs
+++ Pages/Sending.cshtml.cs
@@ -32,34 +32,45 @@ namespace easytransfer.Pages
                 return Page();
             }
 
+            // Check if sender is of type 1
+            var sender = _Context.easyuser.SingleOrDefault(x => x.uid == TransactionHistory.sender);
+            if (sender.usertype != 1)
+            {
+                // Add error message
+                ModelState.AddModelError(string.Empty, "Only normal user can send to normal user");
+                return Page();
+            }
 
-            var data = (from easyuser_ in _Context.easyuser
-                        where easyuser_.uid == TransactionHistory.sender
-                        select easyuser_).SingleOrDefault();
-            
-            EasyUser sender = data;
+            // Check if receiver is of type 1
+            var receiver = _Context.easyuser.SingleOrDefault(x => x.uid == TransactionHistory.receiver);
+            if (receiver.usertype != 1)
+            {
+                // Add error message
+                ModelState.AddModelError(string.Empty, "Only normal user can send to normal user");
+                return Page();
+            }
+
+            // Check if sender has enough balance
+            if (sender.balance < TransactionHistory.amount)
+            {
+                // Add error message
+                ModelState.AddModelError(string.Empty, "Insufficient balance");
+                return Page();
+            }
+
             sender.balance = sender.balance - TransactionHistory.amount;
             _Context.Entry(sender).Property(x => x.balance).IsModified = true;
 
-
-            var data2 = (from easyuser_ in _Context.easyuser
-                        where easyuser_.uid == TransactionHistory.receiver
-                        select easyuser_).SingleOrDefault();
-
-            EasyUser receiver = data2;
             receiver.balance = receiver.balance + TransactionHistory.amount;
             _Context.Entry(receiver).Property(x => x.balance).IsModified = true;
 
             transactionhistory.transactiondate = System.DateTime.Now;
             transactionhistory.transactiontype = 1;
-           
-
 
             var result = _Context.Add(transactionhistory);
             _Context.SaveChanges();
 
             return RedirectToPage("AllUsers");
         }
-
     }
 }
